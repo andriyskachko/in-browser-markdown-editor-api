d92f403f60e48f92eed6750a1447747e1584eb6e
Andriy Skachko <96697847+andriyskachko@users.noreply.github.com>
2022-11-16 22:13:50 +0200

    book module

---
+++ src/books/books.service.ts
@@ -0,0 +1,19 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Book, BookDocument } from './schemas/book.schema';
+import { CreateBookDto } from './dto/create-book.dto';
+
+@Injectable()
+export class BooksService {
+  constructor(@InjectModel(Book.name) private bookModel: Model<BookDocument>) {}
+
+  async create(createBookDto: CreateBookDto): Promise<Book> {
+    const createdBook = new this.bookModel(createBookDto);
+    return createdBook.save();
+  }
+
+  async findAll(): Promise<Book[]> {
+    return this.bookModel.find().exec();
+  }
+}
---
+++ src/books/schemas/book.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type BookDocument = HydratedDocument<Book>;
+
+@Schema()
+export class Book {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  text: string;
+
+  @Prop()
+  author: string;
+
+  // TODO
+  // add translations
+  // add author
+  // add versioning
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
